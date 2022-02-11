f9890df3b58f745c561d6b0cff5266175b32244c
Daniela RT <72284123+Daniela9903@users.noreply.github.com>
2022-02-11 13:26:41 -0600

    Se agregaron 3 botones

--- Ejercicios 2/Ejercicio 1/js/index.js
+++ Ejercicios 2/Ejercicio 1/js/index.js
@@ -26,5 +26,28 @@ button2.addEventListener('click', function(){
 
    let colorboton = document.getElementById('boton2');
 
-   boton2.style.background = "red";
+   boton2.style.background = "#3fb3b3";
+})
+
+
+let button3 = document.getElementById('boton3');
+
+button3.addEventListener('click', function(){
+   
+
+   let colorboton2 = document.getElementById('boton3');
+
+   boton3.style.background = "#056997";
+})
+
+
+
+let button4 = document.getElementById('boton4');
+
+button4.addEventListener('click', function(){
+   
+
+   let colorboton3 = document.getElementById('boton4');
+
+   boton4.style.background = "#1e4a5f";
 })
